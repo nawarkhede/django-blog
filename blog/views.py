104cce252b4ecf44d01bd683983368da9492e3eb
Nishant Nawarkhede <ninawark@cisco.com>
2023-10-07 15:47:41 +0530

    code formatted

--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,12 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def post_list(request):
     posts = Post.published_manager.all()
     paginator = Paginator(posts, 2)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
         posts = paginator.page(1)
-    except (EmptyPage):
+    except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
